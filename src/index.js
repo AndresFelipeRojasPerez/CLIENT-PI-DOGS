c021e0347b8ef75378b556963f7331f9f2c17fea
Andres Felipe Rojas Perez <you@example.com>
2023-05-04 10:29:38 -0500

    commit_deploy_front

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import {BrowserRouter} from 'react-router-dom'
 import {Provider} from "react-redux";
 import store from './redux/store';
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://server-pi-dogs-production.up.railway.app/';
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 
 ReactDOM.render(
